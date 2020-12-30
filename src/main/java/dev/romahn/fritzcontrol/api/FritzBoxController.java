b9b94ae87131a30e7277abdac69a59b685f4fa58
jonathanromahn <jonathan.romahn@smartcommerce.de>
2020-12-30 13:48:49 +0100

    simplify response checking

--- src/main/java/dev/romahn/fritzcontrol/api/FritzBoxController.java
+++ src/main/java/dev/romahn/fritzcontrol/api/FritzBoxController.java
@@ -8,10 +8,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import retrofit2.HttpException;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -32,19 +31,23 @@ public class FritzBoxController {
                 .build().create(FritzBoxClient.class);
     }
 
-    public List<Device> getDevices() throws Exception {
-
-        Response<ResponseBody> response = fritzBoxClient.getData("kidLis").execute();
-
-        if (response.isSuccessful()) {
-            return parseDeviceTable(response.body().string());
-        } else {
-            throw new HttpException(response);
-        }
+    public List<Device> getDevices() throws IOException {
+        ResponseBody response = CallUtil.executeAndCheck(fritzBoxClient.getData("kidLis"));
+        return parseDeviceTable(response);
     }
 
-    private List<Device> parseDeviceTable(String in) {
-        Document document = Jsoup.parse(in);
+    public List<Device> saveDevices(List<Device> devices) throws IOException {
+
+        Map<String, String> deviceData = devices.stream().collect(Collectors.toMap(d -> "profile:" + d.getId(), Device::getProfile));
+        deviceData.put("apply", "");
+        deviceData.put("oldpage", "/internet/kids_userlist.lua");
+
+        ResponseBody response = CallUtil.executeAndCheck(fritzBoxClient.sendData(deviceData));
+        return parseDeviceTable(response);
+    }
+
+    private List<Device> parseDeviceTable(ResponseBody in) throws IOException {
+        Document document = Jsoup.parse(in.string());
         Elements deviceTable = document.select("[id=uiDevices]");
 
         profiles = parseProfiles(deviceTable);
@@ -69,18 +72,4 @@ public class FritzBoxController {
         return profiles;
     }
 
-    public List<Device> saveDevices(List<Device> devices) throws Exception {
-
-        Map<String, String> deviceData = devices.stream().collect(Collectors.toMap(d -> "profile:" + d.getId(), Device::getProfile));
-        deviceData.put("apply", "");
-        deviceData.put("oldpage", "/internet/kids_userlist.lua");
-
-        Response<ResponseBody> response = fritzBoxClient.sendData(deviceData).execute();
-
-        if (response.isSuccessful()) {
-            return parseDeviceTable(response.body().string());
-        } else {
-            throw new HttpException(response);
-        }
-    }
 }
