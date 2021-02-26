9293efc2421f48db0a302472f54f3880867b28ef
jonathanromahn <jonathan.romahn@smartcommerce.de>
2021-02-26 23:48:21 +0100

    restructure App.java

--- src/main/java/dev/romahn/fritzcontrol/App.java
+++ src/main/java/dev/romahn/fritzcontrol/App.java
@@ -15,20 +15,26 @@ import java.util.Map;
 public class App {
 
     public static void main(String[] args) throws Exception {
+        CommandLine commandLine = parseArgs(args);
+
         App app = new App();
-        app.execute(createConfig(args));
+        app.execute(createFritzControl(commandLine), commandLine);
     }
 
-    private static FritzControl createConfig(String[] args) throws ParseException {
-
+    private static CommandLine parseArgs(String[] args) throws ParseException {
         Options options = new Options();
         options.addRequiredOption("u", "username", true, "Username for FritzBox login");
         options.addRequiredOption("p", "password", true, "Password for FritzBox login");
         options.addOption("url", "url", true, "FritzBox Url, default is http://fritz.box");
         options.addOption("a", "auth", true, "Authentication Strategy, possible values MD5 & PBKDF2, default is MD5");
+        options.addRequiredOption("d", "device", true, "The device on which to set the profile");
+        options.addRequiredOption("p", "profile", true, "The profile to set the device to");
 
         CommandLineParser parser = new DefaultParser();
-        CommandLine commandLine = parser.parse(options, args, true);
+        return parser.parse(options, args, true);
+    }
+
+    private static FritzControl createFritzControl(CommandLine commandLine) {
 
         FritzControl.Builder builder = new FritzControl.Builder();
 
@@ -48,15 +54,18 @@ public class App {
         return builder.build();
     }
 
-    private void execute(FritzControl fritzControl) throws Exception {
+    private void execute(FritzControl fritzControl, CommandLine commandLine) throws Exception {
 
         DeviceController deviceController = new DeviceController(fritzControl.getClient());
 
         Map<String, String> deviceProfiles = new HashMap<>();
-
-        deviceProfiles.put("Jonathan-PC", "Fight The Addiction");
+        String device = commandLine.getOptionValue("device");
+        String profile = commandLine.getOptionValue("profile");
+        deviceProfiles.put(device, profile);
 
         deviceController.setProfilesForDevices(deviceProfiles);
+
+        System.out.println("Set " + device + " to " + profile);
     }
 
 }
